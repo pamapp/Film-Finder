363ad4b5990a76b3b878a1ff4b3200fa65ddb3eb
alina-potapova <alineus@mail.ru>
2020-06-03 00:14:48 +0500

    Initial Commit

---
+++ filmRandom/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  filmRandom
+//
+//  Created by Alina Potapova on 03.06.2020.
+//  Copyright © 2020 Pamapp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
